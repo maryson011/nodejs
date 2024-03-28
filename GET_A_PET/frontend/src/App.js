5a7d52d331b559dd9b2e7b327c8fa0a9cf3b0d7c
maryson gleison <maryson011@gmail.com>
2024-03-28 15:26:01 -0300

    iniciado frontend da aplicação. Configuração de ambiente, correção do react-router-dom para adapitar a versão atual com a do curso, criado componentes principais da pagina.

---
+++ GET_A_PET/frontend/src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+/* Components */
+import Navbar from './components/layouts/Navbar' 
+import Footer from './components/layouts/Footer' 
+
+/* Pages */
+import Login from './components/pages/Auth/Login'
+import Register from './components/pages/Auth/Register'
+import Home from './components/pages/Home'
+
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/register' element={<Register />}/>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ GET_A_PET/frontend/src/components/layouts/Footer.js
@@ -0,0 +1,11 @@
+function Footer(){
+    return (
+        <footer>
+            <p>
+                <span>Get A Pet</span> &copy; 2024
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
