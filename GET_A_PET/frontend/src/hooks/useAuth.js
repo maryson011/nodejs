1e013670bf74798b49fe782fd85d49f7bc64ad39
maryson gleison <maryson011@gmail.com>
2024-04-02 11:34:18 -0300

    finalizado funcionalidade de login e testado

--- GET_A_PET/frontend/src/hooks/useAuth.js
+++ GET_A_PET/frontend/src/hooks/useAuth.js
@@ -45,6 +45,25 @@ export default function useAuth(){
         navigate('/')
     }
 
+    async function login(user){
+        let msgText = 'Login realizado com sucesso!'
+        let msgType = 'success'
+
+        try{
+
+            const data = await api.post('/users/login', user).then((response)=> {
+                return response.data
+            })
+
+            await authUser(data)
+        }catch(error){
+            msgText = error.response.data.message
+            msgType = 'error'
+        }
+
+        setFlashMessage(msgText, msgType)
+    }
+
     function logout(){
         const msgText = 'Logout realizado com sucesso!'
         const msgType = 'success'
@@ -57,5 +76,5 @@ export default function useAuth(){
         setFlashMessage(msgText, msgType)
     }
 
-    return { authenticated, register, logout }
+    return { authenticated, register, logout, login }
 }
