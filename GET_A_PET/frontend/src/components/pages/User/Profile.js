d174582b8577c3a833ba74c48198a2b3a969a4e5
maryson gleison <maryson011@gmail.com>
2024-04-02 11:57:16 -0300

    Finalizado layout do formulario de perfil

---
+++ GET_A_PET/frontend/src/components/pages/User/Profile.js
@@ -0,0 +1,76 @@
+import { useState, useEffect } from 'react'
+
+import styles from './Profile.module.css'
+import formStyles from '../../form/Form.module.css'
+
+import Input from '../../form/input'
+
+
+function Profile(){
+    const [user, setUser] = useState({})
+
+    function onFileChange(e){
+
+    }
+
+    function handleChange(e){}
+
+    return (
+        <section>
+            <div className={styles.profile_header}>
+                <h1>Perfil</h1>
+                <p>Preview Imagem</p>
+            </div>
+            <form className={formStyles.form_container}>
+                <Input
+                    text="Imagem"
+                    type="file"
+                    name="image"
+                    handleOnChange={onFileChange}
+                />
+                <Input
+                    text="E-mail"
+                    type="email"
+                    name="email"
+                    placeholder="Digite seu e-mail"
+                    handleOnChange={handleChange}
+                    value={user.email || ''}
+                />
+                <Input
+                    text="Nome"
+                    type="text"
+                    name="name"
+                    placeholder="Digite o seu nome"
+                    handleOnChange={handleChange}
+                    value={user.name || ''}
+                />
+                <Input
+                    text="Telefone"
+                    type="text"
+                    name="phone"
+                    placeholder="Digite o seu telefone"
+                    handleOnChange={handleChange}
+                    value={user.phone || ''}
+                />
+                <Input
+                    text="Senha"
+                    type="password"
+                    name="password"
+                    placeholder="Digite a sua senha"
+                    handleOnChange={handleChange}
+                    value={user.password || ''}
+                />
+                <Input
+                    text="Confirmação de Senha"
+                    type="password"
+                    name="confirmpassword"
+                    placeholder="Confirme a sua senha"
+                    handleOnChange={handleChange}
+                />
+                <input type='submit' value="Editar" />
+            </form>
+        </section>
+    )
+}
+
+export default Profile
