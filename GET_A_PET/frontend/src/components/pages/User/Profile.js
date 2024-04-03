dc6e2f1f1d2017acc64802a9de59141f78e4a280
maryson gleison <maryson011@gmail.com>
2024-04-03 09:48:11 -0300

    configurado a seleção de imagen do perfil e atualização, assim como a apresentação da mesma

--- GET_A_PET/frontend/src/components/pages/User/Profile.js
+++ GET_A_PET/frontend/src/components/pages/User/Profile.js
@@ -6,11 +6,16 @@ import styles from './Profile.module.css'
 import formStyles from '../../form/Form.module.css'
 
 import Input from '../../form/input'
+import RoundedImage from '../../layouts/Roundedimage'
+
+import useFlashMessage from '../../../hooks/useFlashMessage'
 
 
 function Profile(){
     const [user, setUser] = useState({})
+    const [preview, setPreview] = useState()
     const [token] = useState(localStorage.getItem('token') || '')
+    const { setFlashMessage } = useFlashMessage()
 
     useEffect(()=> {
         api.get('/users/checkuser', {
@@ -23,18 +28,53 @@ function Profile(){
     }, [token])
 
     function onFileChange(e){
-
+        setPreview(e.target.files[0])
+        setUser({ ...user, [e.target.name]: e.target.files[0] })
     }
 
-    function handleChange(e){}
+    function handleChange(e){
+        setUser({ ...user, [e.target.name]: e.target.value })
+    }
+
+    async function handleSubmit(e){
+        e.preventDefault()
+        
+        let msgType = 'success'
+
+        const formData = new FormData()
+
+        await Object.keys(user).forEach((key)=> {
+            formData.append(key, user[key])
+        })
+
+        const data = await api
+            .patch(`/users/edit/${user._id}`, formData, {
+                headers: {
+                    Authorization: `Bearer ${JSON.parse(token)}`,
+                    'Content-Type': 'multipart/form-data',
+                },
+            })
+            .then((response)=> {
+                console.log(response.data)
+                return response.data
+            })
+            .catch((error)=> {
+                msgType = 'error'
+                return error.response.data
+            })
+
+            setFlashMessage(data.message, msgType)
+    }
 
     return (
         <section>
             <div className={styles.profile_header}>
                 <h1>Perfil</h1>
-                <p>Preview Imagem</p>
+                {(user.image || preview) && (
+                    <RoundedImage src={preview ? URL.createObjectURL(preview) : `${process.env.REACT_APP_API}/imagens/users/${user.image}`} alt={user.name} />
+                )}
             </div>
-            <form className={formStyles.form_container}>
+            <form onSubmit={handleSubmit} className={formStyles.form_container}>
                 <Input
                     text="Imagem"
                     type="file"
