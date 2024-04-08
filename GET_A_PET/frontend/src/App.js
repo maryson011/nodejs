d949ec155bc31b99b3934ac8be9dc004ff1cb4d6
maryson gleison <maryson011@gmail.com>
2024-04-08 10:17:38 -0300

    concluido adoção e fim do ciclo no sistema - uffa

--- GET_A_PET/frontend/src/App.js
+++ GET_A_PET/frontend/src/App.js
@@ -15,6 +15,7 @@ import MyPets from './components/pages/Pets/MyPets'
 import AddPet from './components/pages/Pets/AddPet'
 import EditPet from './components/pages/Pets/EditPets'
 import PetDetails from './components/pages/Pets/PetDetails'
+import MyAdoptions from './components/pages/Pets/MyAdoptions'
 
 /* context */
 import { UserProvider } from './context/UserContext'
@@ -30,12 +31,13 @@ function App() {
         <Routes>
           <Route path='/login' element={<Login />}/>
           <Route path='/register' element={<Register />}/>
+          <Route path='/' element={<Home />} />
           <Route path='/user/profile' element={<Profile />}/>
           <Route path='/pet/mypets' element={<MyPets />}/>
           <Route path='/pet/add' element={<AddPet />}/>
           <Route path='/pet/edit/:id' element={<EditPet />}/>
+          <Route path='/pet/myadoptions' element={<MyAdoptions />}/>
           <Route path='/pet/:id' element={<PetDetails />}/>
-          <Route path='/' element={<Home />} />
         </Routes>
       </Container>
       <Footer />
--- GET_A_PET/frontend/src/components/pages/Pets/MyPets.js
+++ GET_A_PET/frontend/src/components/pages/Pets/MyPets.js
@@ -48,6 +48,24 @@ function MyPets(){
         setFlashMessage(data.message, msgType)
     }
 
+    async function concludeAdoption(id){
+        let msgType = 'success'
+        const data = await api.patch(`/pets/conclude/${id}`, {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(token)}`,
+            }
+        })
+        .then((response)=> {
+            return response.data
+        })
+        .catch((err)=> {
+            msgType = 'error'
+            return err.response.data
+        })
+
+        setFlashMessage(data.message, msgType)
+    }
+
     return (
         <section>
             <div className={styles.petslist_header}>
@@ -68,7 +86,9 @@ function MyPets(){
                                 {pet.available ? (
                                     <>
                                         {pet.adopter && (
-                                            <button className={styles.conclude_btn}>
+                                            <button className={styles.conclude_btn} onClick={()=> {
+                                                concludeAdoption(pet._id)
+                                            }}>
                                                 Concluir adoção
                                             </button>
                                         )}
